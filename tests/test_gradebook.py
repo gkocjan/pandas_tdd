2bc010cae1d270b1f631b89c7df8b468e95a2f2c
Grzegorz Kocjan <grzegorz@kocjan.me>
2022-09-21 23:57:46 +0200

    [RED] Second test for creating grouped result

--- tests/test_gradebook.py
+++ tests/test_gradebook.py
@@ -5,7 +5,7 @@ def generate_grade_book(students_df: pd.DataFrame) -> dict[int, pd.DataFrame]:
     return {1: pd.DataFrame()}
 
 
-def test_results_are_grouped_by_student_group():
+def test_results_are_grouped_by_student_group_for_students_in_one_group():
     students = [
         {
             "ID": 1,
@@ -20,3 +20,27 @@ def test_results_are_grouped_by_student_group():
     result = generate_grade_book(students_df=students_df)
 
     assert list(result.keys()) == [1]
+
+
+def test_results_are_grouped_by_student_group_for_students_in_multiple_groups():
+    students = [
+        {
+            "ID": 1,
+            "Name": "Doe, John",
+            "NetID": "JXD12345",
+            "Email Adress": "JOHN.DOE@EXAMPLE.EDU",
+            "Group": 1,
+        },
+        {
+            "ID": 2,
+            "Name": "Doe, Second",
+            "NetID": "SXD54321",
+            "Email Adress": "SECOND.DOE@EXAMPLE.EDU",
+            "Group": 2,
+        },
+    ]
+    students_df = pd.DataFrame(data=students).set_index("ID")
+
+    result = generate_grade_book(students_df=students_df)
+
+    assert list(result.keys()) == [1, 2]
