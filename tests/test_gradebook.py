ddf8a75ce1d0ab7db3d45da9f4c3dc5724322ea1
Grzegorz Kocjan <grzegorz@kocjan.me>
2022-09-21 23:30:06 +0200

    [RED] Prepare the first input data
    We exactly know how the input data looks like so in our first test we
    can start recreating it manually.

--- tests/test_gradebook.py
+++ tests/test_gradebook.py
@@ -1,2 +1,16 @@
+import pandas as pd
+
+
 def test_x():
+    students = [
+        {
+            "ID": 1,
+            "Name": "Doe, John",
+            "NetID": "JXD12345",
+            "Email Adress": "JOHN.DOE@EXAMPLE.EDU",
+            "Group": 1,
+        }
+    ]
+    students_df = pd.DataFrame(data=students).set_index("ID")
+
     assert False
