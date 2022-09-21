75c9b38f88e6f9b164a8dbcaf402bec0d236e92c
Grzegorz Kocjan <grzegorz@kocjan.me>
2022-09-21 23:48:29 +0200

    [RED] Extract generate grade book function

--- tests/test_gradebook.py
+++ tests/test_gradebook.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 
+def generate_grade_book(students_df: pd.DataFrame) -> dict[int, pd.DataFrame]:
+    return {}
+
+
 def test_results_are_grouped_by_student_group():
     students = [
         {
@@ -13,6 +17,6 @@ def test_results_are_grouped_by_student_group():
     ]
     students_df = pd.DataFrame(data=students).set_index("ID")
 
-    result = {}
+    result = generate_grade_book(students_df=students_df)
 
     assert list(result.keys()) == [1]
