74b8b613cdc9cb1b15c532070c80e4d365e1dfa2
Grzegorz Kocjan <grzegorz@kocjan.me>
2022-09-22 23:04:12 +0200

    [ALERT!] Failed refactoring start
    One of refactoring techniques is to combine everything into one function
    - regrouping the code. Unfortunately even with simple code it's easy to
    make mistakes. Nowe almost all test have exploded. We need to fix them.

--- gradebook/main.py
+++ gradebook/main.py
@@ -2,50 +2,6 @@ import pandas as pd
 from typing import cast
 
 
-def _create_group(
-    students_with_scores: pd.DataFrame, max_quiz_scores: dict
-) -> pd.DataFrame:
-    result = pd.DataFrame(index=students_with_scores.index)
-    result = result.assign(
-        net_id=students_with_scores.index,
-        email_address=students_with_scores["Email Address"].str.lower(),
-    )
-    result[["last_name", "first_name"]] = students_with_scores["Name"].str.split(
-        ", ", expand=True
-    )
-
-    homework_scores = students_with_scores.filter(
-        regex=r"^homework_\d\d?$",
-        axis=1,
-    )
-    homework_max_points = students_with_scores.filter(
-        regex=r"^homework_\d\d?_max_points$",
-        axis=1,
-    ).set_axis(homework_scores.columns, axis=1)
-    sum_of_homework_averages = (homework_scores / homework_max_points).sum(axis=1)
-    number_of_homeworks = homework_scores.shape[1]
-
-    result["homework_score"] = sum_of_homework_averages / number_of_homeworks
-
-    number_of_exams = students_with_scores.filter(regex=r"^exam_\d\d?$", axis=1).shape[
-        1
-    ]
-
-    for exam_numer in range(1, number_of_exams + 1):
-        result[f"exam_{exam_numer}_score"] = (
-            students_with_scores[f"exam_{exam_numer}"]
-            / students_with_scores[f"exam_{exam_numer}_max_points"]
-        )
-
-    sum_of_quiz_scores = students_with_scores.filter(regex=r"^quiz_\d$", axis=1).sum(
-        axis=1
-    )
-    sum_of_quiz_max = sum(max_quiz_scores.values())
-    result["quiz_score"] = (sum_of_quiz_scores / sum_of_quiz_max).round(2)
-
-    return result
-
-
 def generate_gradebook(
     students_df: pd.DataFrame,
     homework_exams_df: pd.DataFrame,
@@ -83,9 +39,43 @@ def generate_gradebook(
         right_index=True,
     )
 
-    return {
-        cast(int, group): _create_group(
-            students_with_scores=table, max_quiz_scores=max_quiz_scores
+    result = pd.DataFrame(index=students_with_scores.index)
+    result = result.assign(
+        net_id=students_with_scores.index,
+        group=students_with_scores["Group"],
+        email_address=students_with_scores["Email Address"].str.lower(),
+    )
+    result[["last_name", "first_name"]] = students_with_scores["Name"].str.split(
+        ", ", expand=True
+    )
+
+    homework_scores = students_with_scores.filter(
+        regex=r"^homework_\d\d?$",
+        axis=1,
+    )
+    homework_max_points = students_with_scores.filter(
+        regex=r"^homework_\d\d?_max_points$",
+        axis=1,
+    ).set_axis(homework_scores.columns, axis=1)
+    sum_of_homework_averages = (homework_scores / homework_max_points).sum(axis=1)
+    number_of_homeworks = homework_scores.shape[1]
+
+    result["homework_score"] = sum_of_homework_averages / number_of_homeworks
+
+    number_of_exams = students_with_scores.filter(regex=r"^exam_\d\d?$", axis=1).shape[
+        1
+    ]
+
+    for exam_numer in range(1, number_of_exams + 1):
+        result[f"exam_{exam_numer}_score"] = (
+            students_with_scores[f"exam_{exam_numer}"]
+            / students_with_scores[f"exam_{exam_numer}_max_points"]
         )
-        for group, table in students_with_scores.groupby("Group")
-    }
+
+    sum_of_quiz_scores = students_with_scores.filter(regex=r"^quiz_\d$", axis=1).sum(
+        axis=1
+    )
+    sum_of_quiz_max = sum(max_quiz_scores.values())
+    result["quiz_score"] = (sum_of_quiz_scores / sum_of_quiz_max).round(2)
+
+    return {cast(int, group): table for group, table in result.groupby("Group")}
